efc96950cd1d71b0e1db14df3302a428e41ea0e0
Xiaocun Que <q.xiaocun@gmail.com>
2016-07-18 22:29:42 -0700

    Invert a binary tree.
    This problem was inspired by this original tweet by Max Howell:
    Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.

---
+++ invert_binary_tree_226.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        # Interpolation
+        return "%s [%s,%s]" % (self.val,self.left,self.right)
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
+if __name__ == '__main__':
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(9)
+    print root
+    s=Solution()
+    print s.invertTree(root)
